3dd5b852db7611c128a009aba3725a43222e87d5
Ma'murjon <xayrullayevmamur381@gmail.com>
2025-08-05 22:31:16 +0500

    yangi

--- e_kurs/chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ChatView, ChatHistoryView
-
-urlpatterns = [
-    path('', ChatView.as_view(), name='chat'),
-
-    path('history/', ChatHistoryView.as_view(), name='chat-history'),
-]
--- e_kurs/payment/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import CreateChargeView
-
-urlpatterns = [
-    path('charge/', CreateChargeView.as_view(), name='create-charge'),
-]
